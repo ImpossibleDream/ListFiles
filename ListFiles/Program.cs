c043c3d275170edb64f132ec1eb21fc3a0aa3013
Fan Jing <jing1999fan@outlook.com>
2022-04-13 01:25:52 -0700

    Add project files.

---
+++ ListFiles/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace ListFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String path = "C://Users/admin/DeskTop/Daily Report";
+            DirectoryInfo folder = new DirectoryInfo(path);
+            foreach (FileInfo file in folder.GetFiles("*.txt"))
+            {
+                Console.WriteLine(file.FullName);
+            }
+            Console.ReadKey();
+        }
+    }
+}
